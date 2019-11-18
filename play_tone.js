deca74567ea8b81116269a3155f26c77a1435c71
Chris Dugan <c.m.s.dugan@gmail.com>
2019-11-18 10:57:07 -0500

    Adjust tone

--- play_tone.js
+++ play_tone.js
@@ -1,7 +1,18 @@
 
 function playTone(note) {
 	console.log("playTone(" + note + ")")
-	var synth = new Tone.Synth().toMaster();
+	var synth = new Tone.Synth(
+		{
+		  oscillator: {
+		    type: 'sine'
+		  },
+		  envelope: {
+		    attack: 2,
+		    sustain: 2,
+		    release: 0.1
+		  }
+		}
+	).toMaster();
 	if (Tone.context.state !== 'running') {
         Tone.context.resume();
     }
